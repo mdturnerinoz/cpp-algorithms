1511e903f752518e5aa830f49c62669077d12e14
Marty Turner <mdturnerinoz@gmail.com>
2014-01-18 16:22:34 +1100

    minor formatting

--- cpp-algorithms.cpp
+++ cpp-algorithms.cpp
@@ -1,52 +1,53 @@
+//
+//  C++ sample algirthm code from The Geek Stuff (TGS) article at
+//  http://www.thegeekstuff.com/2014/01/cpp-binary-exercise/?utm_source=feedburner&utm_medium=email&utm_campaign=Feed%3A+TheGeekStuff+%28The+Geek+Stuff%29
+//
+
 #include <iostream>
 
 using namespace std;
 
-void Display( int , int, int);
+void Display(int, int, int);
 
-int
-main(void) {
+int main(void)
+{
 
-    cout<<"How many digits in the binary number->";
-    int iN;
-    cin>>iN;
+   cout << "How many digits in the binary number->";
+   int iN;
+   cin >> iN;
 
-    //Start position for left and right marker
-    int i= iN-1,
-        j= iN;
-    while(!((i==1)&&(j==2)))
-    {
-        Display( i, j, iN);
+   //Start position for left and right marker
+   int i = iN - 1,
+      j = iN;
+   while (!((i == 1) && (j == 2)))
+   {
+      Display(i, j, iN);
 
-        if(i==j-1)
-        {
-            i--;
-            j=iN;
-            cout<<endl;
-        }
-        else
-        {
-            j--;
-        }
-    }
+      if (i == j - 1)
+      {
+         i--;
+         j = iN;
+         cout << endl;
+      }
+      else
+      {
+         j--;
+      }
+   }
 
-    cout<<"11";
-    for(int i=2; i<iN; cout<<'0',i++);
-    cout<<endl;
+   cout << "11";
+   for (int i = 2; i < iN; cout << '0', i++) ;
+   cout << endl;
 
-    int iEnd;
-    cin>>iEnd;
+   int iEnd;
+   cin >> iEnd;
 
-    return EXIT_SUCCESS;
+   return EXIT_SUCCESS;
 }
 
-void
-Display( int i,int j,int n)
+void Display(int i, int j, int n)
 {
-    for(int k=1; k<= n; k++)
-        if( (k==i)||(k==j))
-            cout<<'1';
-        else
-            cout<<'0';
-    cout<<endl;
+   for (int k = 1; k <= n; k++) if ((k == i) || (k == j)) cout << '1';
+      else cout << '0';
+   cout << endl;
 }
