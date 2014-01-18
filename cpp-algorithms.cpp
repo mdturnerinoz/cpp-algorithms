2a43a5e6b6bbfd1566f22b977c3f57ce48d2d06f
Marty Turner <mdturnerinoz@gmail.com>
2014-01-18 15:52:20 +1100

    ran astyle against the code

--- cpp-algorithms.cpp
+++ cpp-algorithms.cpp
@@ -5,44 +5,48 @@ using namespace std;
 void Display( int , int, int);
 
 int
-main(void){
+main(void) {
 
-	cout<<"How many digits in the binary number->";
-	int iN; cin>>iN;
+    cout<<"How many digits in the binary number->";
+    int iN;
+    cin>>iN;
 
-	//Start position for left and right marker
-	int i= iN-1,
-	    j= iN;
-	while(!((i==1)&&(j==2)))
-	{
-		Display( i, j, iN);
+    //Start position for left and right marker
+    int i= iN-1,
+        j= iN;
+    while(!((i==1)&&(j==2)))
+    {
+        Display( i, j, iN);
 
-		if(i==j-1)
-		{
-			i--; j=iN; cout<<endl;
-		}
-		else
-		{
-			j--;
-		}
-	}
+        if(i==j-1)
+        {
+            i--;
+            j=iN;
+            cout<<endl;
+        }
+        else
+        {
+            j--;
+        }
+    }
 
- cout<<"11";
- for(int i=2; i<iN; cout<<'0',i++);
- cout<<endl;
+    cout<<"11";
+    for(int i=2; i<iN; cout<<'0',i++);
+    cout<<endl;
 
- int iEnd; cin>>iEnd;
+    int iEnd;
+    cin>>iEnd;
 
- return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
 
-void 
+void
 Display( int i,int j,int n)
 {
-	for(int k=1; k<= n; k++)
-		if( (k==i)||(k==j))
-			cout<<'1';
-		else
-			cout<<'0';
-	cout<<endl;
+    for(int k=1; k<= n; k++)
+        if( (k==i)||(k==j))
+            cout<<'1';
+        else
+            cout<<'0';
+    cout<<endl;
 }
