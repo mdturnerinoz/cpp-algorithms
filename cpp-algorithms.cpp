c9d83f2029a43c83eea1e06f5f7da1c7346cc709
Marty Turner <mdturnerinoz@gmail.com>
2014-01-18 16:37:11 +1100

    more commentary

--- cpp-algorithms.cpp
+++ cpp-algorithms.cpp
@@ -2,12 +2,24 @@
 //  C++ sample algirthm code from The Geek Stuff (TGS) article at
 //  http://www.thegeekstuff.com/2014/01/cpp-binary-exercise/?utm_source=feedburner&utm_medium=email&utm_campaign=Feed%3A+TheGeekStuff+%28The+Geek+Stuff%29
 //
+// The code has been reformatted for readability and at least one bug fixed (where the code hangs at the end).
+//
 
 #include <iostream>
 
 using namespace std;
 
-void Display(int, int, int);
+void Display(int i, int j, int n)
+{
+   for (int k = 1; k <= n; k++) 
+   {
+      if ((k == i) || (k == j)) 
+            cout << '1';
+       else cout << '0';
+   }
+
+   cout << endl;
+}
 
 int main(void)
 {
@@ -19,6 +31,7 @@ int main(void)
    //Start position for left and right marker
    int i = iN - 1,
       j = iN;
+
    while (!((i == 1) && (j == 2)))
    {
       Display(i, j, iN);
@@ -45,9 +58,4 @@ int main(void)
    return EXIT_SUCCESS;
 }
 
-void Display(int i, int j, int n)
-{
-   for (int k = 1; k <= n; k++) if ((k == i) || (k == j)) cout << '1';
-      else cout << '0';
-   cout << endl;
-}
+
